a68f956875c104e8394be43102038696391dcf1e
MOSALAHDEV <mosalah525446@gmail.com>
2024-09-13 12:19:52 -0700

    code completed to print different parts of string

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = word[:3]
 print(f"First 3 letters: {word_first_3}")
-word_last_2 = word[7:]
+word_last_2 = word[-2:]
 print(f"Last 2 letters: {word_last_2}")
-middle_word = word[1:8]
+middle_word = word[1:-1]
 print(f"Middle word: {middle_word}")
