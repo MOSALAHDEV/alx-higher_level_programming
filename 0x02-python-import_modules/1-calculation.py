ab0f4d4d9035d3b2a362e6c4f02db8088a673d73
MOSALAHDEV <mosalah525446@gmail.com>
2024-10-07 04:49:25 -0700

    Add calculator program that imports and uses functions from calculator_1.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,5 +4,6 @@ a = 10
 b = 5
 print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
 print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+if __name__ == "__main__":
+    pass
