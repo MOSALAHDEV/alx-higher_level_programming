e5af3c9e9eb87f964f16e56198adea7b1bd42b91
MOSALAHDEV <mosalah525446@gmail.com>
2024-10-16 12:54:54 -0700

    Add program to list number and details of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+arguments = sys.argv[1:]
+number = len(arguments)
+if number == 0:
+    print("0 arguments")
+elif number == 1:
+    print("1 argument:")
+    print("1: {}".format(arguments[0]))
+else:
+    print("{} arguments:".format(number))
+    for i, argument in enumerate(arguments, start=1):
+        print("{}: {}".format(i, argument))
