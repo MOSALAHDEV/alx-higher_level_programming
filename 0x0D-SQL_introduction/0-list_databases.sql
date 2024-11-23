4e8b7c998ee7e342a971c12b12bfb6dc89ba906a
MOSALAHDEV <mosalah525446@gmail.com>
2024-11-23 13:20:50 -0800

    Added a script that lists all databases of your MySQL server.

---
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all databases of your MySQL server.
+-- Query to display all databases on MySql server
+SHOW DATABASES;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,2 @@
+# SQL Project
+this project contains SQL scripts designed to run on MySql 8.0
