2fd7f97af80f1264fe549316dfca63ef455a717c
MOSALAHDEV <mosalah525446@gmail.com>
2024-11-24 12:45:03 -0800

    Added a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,12 @@
 -- Create table second_table if not exists
 CREATE TABLE IF NOT EXISTS second_table(
-	id INT AUTO_INCREMENT PRIMARY KEY,	-- define id as primary key
+	id INT PRIMARY KEY,	-- define id as primary key
 	name VARCHAR(256),	-- define name to store strings
 	score INT		-- define score to store integers
 );
 -- Insert data into second_table
-INSERT INTO second_table(name, score) VALUES('John', 10);
-INSERT INTO second_table(name, score) VALUES('Alex', 3);
-INSERT INTO second_table(name, score) VALUES('Bob', 14);
-INSERT INTO second_table(name, score) VALUES('George', 8);
+-- use ignore to avoid error
+INSERT IGNORE INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT IGNORE INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT IGNORE INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT IGNORE INTO second_table (id, name, score) VALUES (4, 'George', 8);
