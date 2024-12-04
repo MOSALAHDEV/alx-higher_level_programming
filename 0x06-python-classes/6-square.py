f0c81d62196699b198ad79654930e0be027b1f78
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-03 22:32:23 -0800

    Fix validation in setter method for position attribute

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -107,7 +107,9 @@ class Square:
         """
         if not isinstance(value, tuple):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2 and not all(isinstance(i, int)for i in value):
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(i, int)for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 and value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
