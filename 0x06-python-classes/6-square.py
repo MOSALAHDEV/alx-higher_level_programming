ac649af850f69f79239456e63b7283fbb1f99bee
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-05 04:42:59 -0800

    Modified the validation of the getter method of position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -102,7 +102,7 @@ class Square:
                 TypeError
         """
         if not isinstance(value, tuple) or len(value) != 2 or not all(
-            isinstance(i, int) for i in value):
+                isinstance(i, int) for i in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
