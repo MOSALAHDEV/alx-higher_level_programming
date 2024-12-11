9129e90743bff13d6967fd63ef574de59b2f46b2
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-11 00:54:58 -0800

    Added function that divides a matrix by a div after validation checking

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -23,35 +23,15 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not isinstance(matrix, list) or #!/usr/bin/python3
-"""
-This module contains a function that divides elements of matrix by a number
-The function checks if the matrix is a list of lists of integers or flaots
-checks if the lists of matrix of the same length
-Functions:
-    matrix_divided(matrix, div): divides each element of the matrix by div
-Exceptions:
-    TypeError and ZeroDivisionError
-"""
-
-
-def matrix_divided(matrix, div):
-    """
-    divides all elements of matrix by div
-    args:
-        matrix(list of lists)
-        div(int or float)
-    Return:
-        new matrix with each element divided by div
-    """
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
     if len(matrix) == 1:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
     if not matrix:
         return []
     row_length = len(matrix[0])
@@ -60,37 +40,17 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        new_matrix = []
-        for row in matrix:
-            new_row = []
-            for element in row:
-                if element == float('inf') or
-                element == float('-inf') or element != element:
-                    new_row.append(0.0)
-                else:
-                    new_row.append(round(element / div, 2))
-            new_matrix.append(new_row)
-    return new_matrixnot all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if len(matrix) == 1:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if not matrix:
-        return []
-    row_length = len(matrix[0])
+                raise TypeError("""matrix must be a matrix
+                (list of lists) of integers/floats""")
+    new_matrix = []
     for row in matrix:
-        if len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
         for element in row:
-            if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        new_matrix = []
-        for row in matrix:
-            new_row = []
-            for element in row:
-                if element == float('inf') or element == float('-inf') or element != element:
-                    new_row.append(0.0)
-                else:
-                    new_row.append(round(element / div, 2))
-            new_matrix.append(new_row)
+            if element == float('inf') or element == float('-inf'):
+                new_row.append(0.0)
+            elif element != element:
+                new_row.append(0.0)
+            else:
+                new_row.append(round(element / div, 2))
+        new_matrix.append(new_row)
     return new_matrix
