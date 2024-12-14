8b6f722cc883fd9233c20af283e861678927d514
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-14 00:43:37 -0800

    Added a script to print fullname with tests

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module prints first name and last name.
+first name and last name must be strings
+if not strings, raise TyepeError first_name must be
+a string or last_name must be a string
+
+function name say_my_name(first_name, last_name="")
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function prints first name and last name.
+    first name and last name must be strings
+    if not strings, raise TyepeError first_name must be
+    a string or last_name must be a string
+
+    args:
+        first_name(string)
+        last_name(string)
+    returns:
+        a string with first and last name
+    raises:
+        TypeError: if first or last name are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    return f"My name is {first_name} {last_name}".strip()
