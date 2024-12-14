31beb4df232d094eb7f92ad9c044a4b5d64f6388
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-14 09:17:41 -0800

    added

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -15,9 +15,10 @@ def print_square(size):
     Returns:
         None
     """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    for _ in range(size):
-        print("#" * size)
+    if size is not None:
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        for _ in range(size):
+            print("#" * size)
