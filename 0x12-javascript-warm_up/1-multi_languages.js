7c2f55de539807a04f250a948a85692916341a24
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-18 01:35:16 -0800

    Added a script to print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const cLanguage = 'C is fun';
+const pythonLanguage = 'Python is cool';
+const javascriptLanguage = 'Javascript is amazing';
+const languages = [cLanguage, pythonLanguage, javascriptLanguage];
+console.log(`${languages[0]}\n${languages[1]}\n${languages[2]}`);
