caaf5827b4f12b94752df6b7e21b7447a3ccb86d
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-18 01:39:58 -0800

    Added a script to print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,5 +2,4 @@
 const cLanguage = 'C is fun';
 const pythonLanguage = 'Python is cool';
 const javascriptLanguage = 'Javascript is amazing';
-const languages = [cLanguage, pythonLanguage, javascriptLanguage];
-console.log(`${languages[0]}\n${languages[1]}\n${languages[2]}`);
+console.log(`${cLanguage}\n${pythonLanguage}\n${javascriptLanguage}`);
