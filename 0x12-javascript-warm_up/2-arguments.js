b0693d45ddb61ba3b5cd604c5d78146a1ab82e1d
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-18 17:32:49 -0800

    Added a script prints message with number of args passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const message = args.length === 0 ? 'No argument' : args.length === 1 ? 'Argument found' : 'Arguments found';
+console.log(message);
