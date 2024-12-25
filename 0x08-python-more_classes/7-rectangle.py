3c87c318b9c7c62f92073a1a726e65b108fda972
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-25 05:46:15 -0800

    added

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -88,9 +88,8 @@ class Rectangle:
             """
             if self.width == 0 or self.height == 0:
                 return ""
-            return "\n".join(
-            str(self.print_symbol) * self.width for _ in range(self.height)
-        )
+            return "\n".join((self.print_symbol) * [self.width for _ in range(self.height)])
+        
         def __repr__(self):
             """
             This method returns a string representation of the rectangle.
