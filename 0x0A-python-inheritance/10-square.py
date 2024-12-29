29f85e5a5e72638522047a7ed1695908e3b5cd9d
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-28 22:28:38 -0800

    Added class Square that inherits from Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,7 +6,6 @@ This module contains the class Square that inherits from Rectangle
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
     """
     This class inherits from Rectangle
@@ -15,8 +14,8 @@ class Square(Rectangle):
         """
         This function initializes the class
         """
-        super().__init__(size, size)
         self.integer_validator("size", size)
+        super().__init__(size, size) 
         self.__size = size
 
     def __str__(self):
