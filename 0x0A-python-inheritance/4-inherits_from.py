512fc93aa768edea7c2417f0f6c7b0835019d3f6
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-28 18:46:05 -0800

    addded

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module contains the function inherits_from.
+"""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object inherits from a class.
+
+    Returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    """
+    return isinstance(obj, a_class)
