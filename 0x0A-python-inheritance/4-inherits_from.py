4033e3b96bcaefc31554b6cf3cb28e8eba527709
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-28 18:54:48 -0800

    added task 5

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,4 +11,4 @@ def inherits_from(obj, a_class):
     inherited (directly or indirectly) from the specified class;
     otherwise False.
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
