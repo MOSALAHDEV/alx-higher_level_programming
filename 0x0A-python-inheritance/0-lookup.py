e5a2ce81891e8150c9458002e7e948097b296dfb
MOSALAHDEV <mosalah525446@gmail.com>
2024-12-28 17:16:49 -0800

    Added first task

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""
+This module contains the function lookup.
+"""
+
+
 def lookup(obj):
     """returns the list of available attributes and methods of an object"""
     if obj is None:
         return None
-    return dir(obj)
+    return dir(obj) 
