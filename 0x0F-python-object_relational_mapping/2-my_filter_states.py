b7c753f0fc28448f910979df47e895976929bb13
MOSALAHDEV <mosalah525446@gmail.com>
2025-01-07 00:19:22 -0800

    Added script takes one argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Lists states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database)
+    cur = db.cursor()
+    query = """
+    SELECT * FROM states
+    WHERE name = %s
+    ORDER BY states.id ASC"""
+    cur.execute(query, (name,))
+    results = cur.fetchall()
+    for result in results:
+        print(result)
+
+    cur.close()
+    db.close()
